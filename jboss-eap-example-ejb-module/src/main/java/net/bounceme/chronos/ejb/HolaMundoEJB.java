696be6160276f991aab941824cca73e6d11acc44
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-10-09 13:29:39 +0200

    Módulo EJB

---
+++ jboss-eap-example-ejb-module/src/main/java/net/bounceme/chronos/ejb/HolaMundoEJB.java
@@ -0,0 +1,19 @@
+package net.bounceme.chronos.ejb;
+
+import jakarta.ejb.Stateless;
+import org.jboss.logging.Logger;
+
+@Stateless
+public class HolaMundoEJB {
+    
+    private static final Logger logger = Logger.getLogger(HolaMundoEJB.class);
+    
+    public String saludoEJB() {
+        logger.info("EJB: Ejecutando saludoEJB");
+        return "¡Hola desde EJB Stateless!";
+    }
+    
+    public String saludoPersonalizadoEJB(String nombre) {
+        return "¡Hola " + nombre + " desde EJB!";
+    }
+}
