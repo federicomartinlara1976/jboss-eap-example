623015cba74387f46d8d1e070baf3babb353333b
MAMALAFE <f.martin@ibermatica.com>
2025-11-14 12:05:23 +0100

    Update

--- jboss-eap-example/jboss-eap-example-jsf-module/src/main/java/net/bounceme/chronos/bean/UsuarioBean.java
+++ jboss-eap-example/jboss-eap-example-jsf-module/src/main/java/net/bounceme/chronos/bean/UsuarioBean.java
@@ -1,5 +1,7 @@
 package net.bounceme.chronos.bean;
 
+import org.apache.commons.lang3.StringUtils;
+
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -11,25 +13,23 @@ import net.bounceme.chronos.utils.Asserts;
 @Named
 @RequestScoped
 public class UsuarioBean {
-	
+
 	@Getter
 	@Setter
-    private String nombre;
-    
-    @Getter
-    private String mensaje;
-    
-    @Inject
-    @Getter
-    private HolaMundoEJB holaMundoEJB;
-    
-    public void saludar() {
-    	Asserts.assertNotNull(holaMundoEJB, "Error interno");
-    	
-    	if (nombre != null && !nombre.trim().isEmpty()) {
-            mensaje = holaMundoEJB.saludoPersonalizadoEJB(nombre);
-        } else {
-            mensaje = holaMundoEJB.saludoEJB();
-        }
-    }
+	private String nombre;
+
+	@Getter
+	private String mensaje;
+
+	@Inject
+	@Getter
+	private HolaMundoEJB holaMundoEJB;
+
+	public void saludar() {
+		Asserts.assertNotNull(holaMundoEJB, "Error interno");
+
+		mensaje = (StringUtils.isBlank(nombre)) ? 
+				holaMundoEJB.saludoPersonalizadoEJB(nombre) : 
+				holaMundoEJB.saludoEJB();
+	}
 }
