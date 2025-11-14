8c31f068868cd6c49ed9a50f4167d6b42c599e20
MAMALAFE <f.martin@ibermatica.com>
2025-11-14 10:56:41 +0100

    set path

--- jboss-eap-example/jboss-eap-example-rest-module/src/main/java/net/bounceme/chronos/rest/HolaMundoResource.java
+++ jboss-eap-example/jboss-eap-example-rest-module/src/main/java/net/bounceme/chronos/rest/HolaMundoResource.java
@@ -10,7 +10,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/hola")
+@Path("/public/hola")
 public class HolaMundoResource {
     
     @Inject
