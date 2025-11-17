f6ecfcf097b189a1e1c802d4551b45894843af91
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2025-11-17 10:41:32 +0100

    Páginas seguras y método de logout (en pruebas)

--- jboss-eap-example/jboss-eap-example-jsf-module/src/main/java/net/bounceme/chronos/bean/SecurityBean.java
+++ jboss-eap-example/jboss-eap-example-jsf-module/src/main/java/net/bounceme/chronos/bean/SecurityBean.java
@@ -19,8 +19,7 @@ public class SecurityBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@SneakyThrows(IOException.class)
-	public void logout() {
+	public void logout() throws IOException {
 		ExternalContext externalContext = getExternalContext();
 
 		String username = externalContext.getUserPrincipal() != null ? externalContext.getUserPrincipal().getName()
@@ -28,16 +27,66 @@ public class SecurityBean implements Serializable {
 
 		log.infof("🚪 Cerrando sesión para usuario: %s", username);
 
-		// Invalidar sesión
+		// Invalidar sesión local primero
 		externalContext.invalidateSession();
 
-		// Redirigir a logout de Keycloak con redirect_uri
-		String redirectUri = java.net.URLEncoder.encode("http://192.168.1.135:8080/mi-jsf/index.xhtml", "UTF-8");
-		String keycloakLogoutUrl = "http://192.168.1.135:8082/realms/jboss-eap-example/protocol/openid-connect/logout"
-				+ "?redirect_uri=" + redirectUri;
+		try {
+			// Construir URL de logout de Keycloak con los nuevos parámetros
+			String redirectUri = java.net.URLEncoder.encode("http://192.168.1.135:8080/mi-jsf/index.xhtml", "UTF-8");
 
-		log.infof("🔐 Redirigiendo a Keycloak logout: %s", keycloakLogoutUrl);
-		externalContext.redirect(keycloakLogoutUrl);
+			// 🔥 CAMBIO IMPORTANTE: Usar post_logout_redirect_uri en lugar de redirect_uri
+			String keycloakLogoutUrl = "http://192.168.1.135:8082/realms/jboss-eap-example/protocol/openid-connect/logout"
+					+ "?post_logout_redirect_uri=" + redirectUri;
+
+			log.infof("🔐 Redirigiendo a Keycloak logout: %s", keycloakLogoutUrl);
+			externalContext.redirect(keycloakLogoutUrl);
+
+		} catch (Exception e) {
+			log.error("Error durante logout", e);
+			// Fallback: redirigir a la página principal
+			externalContext.redirect("http://192.168.1.135:8080/mi-jsf/index.xhtml");
+		}
+	}
+
+	// Versión mejorada con id_token_hint (recomendada)
+	public void logoutWithToken() throws IOException {
+		ExternalContext externalContext = getExternalContext();
+
+		String username = externalContext.getUserPrincipal() != null ? externalContext.getUserPrincipal().getName()
+				: "unknown";
+
+		log.infof("🚪 Cerrando sesión (con token) para usuario: %s", username);
+
+		try {
+			// Construir URL de logout más robusta
+			String redirectUri = java.net.URLEncoder.encode("http://192.168.1.135:8080/mi-jsf/index.xhtml", "UTF-8");
+
+			// 🔥 URL de logout con ambos parámetros recomendados
+			String keycloakLogoutUrl = "http://192.168.1.135:8082/realms/jboss-eap-example/protocol/openid-connect/logout"
+					+ "?post_logout_redirect_uri=" + redirectUri;
+
+			// Nota: En una aplicación real, aquí se incluiría también el id_token_hint
+			// pero requiere obtener el token de la sesión, lo cual es más complejo
+
+			log.infof("🔐 Redirigiendo a Keycloak logout mejorado: %s", keycloakLogoutUrl);
+
+			// Invalidar sesión local después de construir la URL
+			externalContext.invalidateSession();
+			externalContext.redirect(keycloakLogoutUrl);
+
+		} catch (Exception e) {
+			log.error("Error durante logout con token", e);
+			// Fallback seguro
+			externalContext.invalidateSession();
+			externalContext.redirect("http://192.168.1.135:8080/mi-jsf/index.xhtml");
+		}
+	}
+
+	// Método de logout simple (solo sesión local)
+	public void logoutLocalOnly() throws IOException {
+		ExternalContext externalContext = getExternalContext();
+		externalContext.invalidateSession();
+		externalContext.redirect("http://192.168.1.135:8080/mi-jsf/index.xhtml");
 	}
 
 	public boolean isUserInRole(String role) {
