c2d791f3501f8009b6688cd1925886768dd08288
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2025-12-10 09:05:34 +0100

    Servicio de notificación

---
+++ jboss-eap-example/jboss-eap-example-ejb-module/src/main/java/net/bounceme/chronos/service/impl/NotificationService.java
@@ -0,0 +1,31 @@
+package net.bounceme.chronos.service.impl;
+
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
+import lombok.extern.jbosslog.JBossLog;
+import net.bounceme.chronos.websocket.NotificationWebSocket;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Singleton
+@Startup
+@JBossLog
+public class NotificationService {
+
+    public void sendNotificationToAll(String title, String message) {
+        String jsonNotification = String.format(
+            "{\"type\": \"notification\", \"title\": \"%s\", \"message\": \"%s\", \"timestamp\": \"%s\"}",
+            title.replace("\"", "\\\""),
+            message.replace("\"", "\\\""),
+            LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+        );
+        
+        NotificationWebSocket.broadcast(jsonNotification);
+        log.infof("📤 Notificación enviada a todos los clientes: %s", title);
+    }
+    
+    public void sendSystemNotification(String message) {
+        sendNotificationToAll("Sistema", message);
+    }
+}
